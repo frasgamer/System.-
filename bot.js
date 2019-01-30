598935e2086868b9181e96043f516ee79b6e43ac
Fras#9999 <44925264+frasgamer@users.noreply.github.com>
2019-01-30 17:52:56 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3154,5 +3154,39 @@ channel.guild.owner.send(`<@!${channelremover.id}>
     message.channel.send(id)
 }       });
 
+
+
+
+
+
+
+
+
+const adminprefix = "-";//FRAS
+const devs = ['448964162840690700','307666420743798784'];//FRAS
+client.on('message', message => {//FRAS
+  var argresult = message.content.split(` `).slice(1).join(' ');//FRAS#2729
+    if (!devs.includes(message.author.id)) return;//FRAS
+    
+if (message.content.startsWith(adminprefix + 'py')) {//fras
+  client.user.setGame(argresult);//fras
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)//fras
+} else //fras
+  if (message.content.startsWith(adminprefix + 'name')) {//fras
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'avatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'streaming')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");//wennnn
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+}
+});
+
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
