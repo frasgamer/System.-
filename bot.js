c689543861f7e1b92934bcf25dbeafc022ac60b0
Fras#9999 <44925264+frasgamer@users.noreply.github.com>
2019-01-30 18:04:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1633,23 +1633,7 @@ msg.channel.send(embed).then(() => {
 });
 
 
-client.on('ready', function(){	
-    var ms = 40000 ;	
-    var setGame = ['by Fras#2729','-help'];	
-    var i = -1;	
-    var j = 0;	
-    setInterval(function (){	
-        if( i == -1 ){	
-j = 1;	
-       }	
-        if( i == (setGame.length)-1 ){	
-            j = -1;	
-      }	
-       i = i+j;	
-        client.user.setGame(setGame[i],`http://www.youtube.com/gg`);	
-}, ms);	
-	
-});
+
 
 
 client.on('message', message => {
